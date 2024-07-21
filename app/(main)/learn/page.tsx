530faec92c05af414fdedd9abb1a95c9dbf2f960
Dhaval Dudheliya <dhavaldudheliya77@gmail.com>
2024-07-21 22:14:13 +0530

    make courses page, make userProgress table, make getUserProgress query and getCourses query

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,9 +1,20 @@
+import { redirect } from "next/navigation";
+
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
-import { Header } from "./header";
 import { UserProgress } from "@/components/user-progress";
+import { getUserProgress } from "@/db/queries";
 
-function LearnPage() {
+import { Header } from "./header";
+
+const LearnPage = async () => {
+  const userProgressData = getUserProgress();
+
+  const [userProgress] = await Promise.all([userProgressData]);
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
@@ -19,6 +30,6 @@ function LearnPage() {
       </FeedWrapper>
     </div>
   );
-}
+};
 
 export default LearnPage;
--- db/queries.ts
+++ db/queries.ts
@@ -1,5 +1,8 @@
 import { cache } from "react";
 import db from "./drizzle";
+import { auth } from "@clerk/nextjs/server";
+import { eq } from "drizzle-orm";
+import { userProgress } from "./schema";
 
 
 export const getCourses = cache(async () => {
@@ -7,3 +10,20 @@ export const getCourses = cache(async () => {
 
     return data;
 })
+
+export const getUserProgress = cache(async () => {
+    const {userId} = await auth();
+
+    if(!userId) {
+        return null;
+    }
+
+    const data = await db.query.userProgress.findFirst({
+        where: eq(userProgress.userId, userId),
+        with: {
+            activeCourse: true,
+        },
+    });
+
+    return data;
+});
