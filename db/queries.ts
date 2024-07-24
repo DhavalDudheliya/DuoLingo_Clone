c68655e73c1b18f09ef33d535136a20708529a77
Dhaval Dudheliya <dhavaldudheliya77@gmail.com>
2024-07-24 14:44:22 +0530

    construct UNITs query, edit seed, start making unit component in learn page

--- db/queries.ts
+++ db/queries.ts
@@ -2,19 +2,12 @@ import db from "./drizzle";
 import { cache } from "react";
 import { auth } from "@clerk/nextjs/server";
 import { eq } from "drizzle-orm";
-import { courses, userProgress } from "./schema";
-
-
-export const getCourses = cache(async () => {
-    const data = await db.query.courses.findMany();
-
-    return data;
-})
+import { courses, userProgress, units, challengeProgress } from "./schema";
 
 export const getUserProgress = cache(async () => {
-    const {userId} = await auth();
+    const { userId } = await auth();
 
-    if(!userId) {
+    if (!userId) {
         return null;
     }
 
@@ -28,9 +21,56 @@ export const getUserProgress = cache(async () => {
     return data;
 });
 
+export const getCourses = cache(async () => {
+    const data = await db.query.courses.findMany();
+
+    return data;
+})
+
+export const getUnits = cache(async () => {
+    const { userId } = await auth();
+    const userProgress = await getUserProgress();
+
+    if (!userId || !userProgress?.activeCourseId) {
+        return [];
+    }
+
+    const data = await db.query.units.findMany({
+        where: eq(units.courseId, userProgress.activeCourseId),
+        with: {
+            lessons: {
+                with: {
+                    challenges: {
+                        with: {
+                            challengeProgress: {
+                                where: eq(challengeProgress.userId, userId),
+                            },
+                        }
+                    }
+                }
+            }
+        }
+    });
+
+    const normalizedData = data.map((unit) => {
+        const lessonsWithCompletedStatus = unit.lessons.map((lesson) => {
+            const allCompletedChallenges = lesson.challenges.every((challenge) => {
+                return challenge.challengeProgress
+                    && challenge.challengeProgress.length > 0
+                    && challenge.challengeProgress.every((progress) => progress.completed);
+            });
+            return { ...lesson, completed: allCompletedChallenges }
+        });
+        return { ...unit, lessons: lessonsWithCompletedStatus }
+    });
+
+    return normalizedData;
+});
+
+
 export const getCoursesById = cache(async (courseId: number) => {
     const data = await db.query.courses.findFirst({
-        where:eq(courses.id, courseId),
+        where: eq(courses.id, courseId),
     });
 
     return data;
--- scripts/seed.ts
+++ scripts/seed.ts
@@ -13,6 +13,11 @@ const main = async () => {
 
         await db.delete(schema.courses);
         await db.delete(schema.userProgress);
+        await db.delete(schema.units);
+        await db.delete(schema.lessons);
+        await db.delete(schema.challenges);
+        await db.delete(schema.challengeOptions);
+        await db.delete(schema.challengeProgress);
 
         await db.insert(schema.courses).values([
             {
@@ -34,6 +39,62 @@ const main = async () => {
                 id: 4,
                 title: "Italian",
                 imageSrc: "/it.svg",
+            },
+        ])
+
+        await db.insert(schema.units).values([
+            {
+                id: 1,
+                courseId: 1, // German
+                title: "Units 1",
+                description: "Learn the basics of German",
+                order: 1,
+            }
+        ]);
+
+        await db.insert(schema.lessons).values([
+            {
+                id: 1,
+                unitId: 1,
+                order: 1,
+                title: "Nouns",
+            },
+        ]);
+
+        await db.insert(schema.challenges).values([
+            {
+                id: 1,
+                lessonId: 1, // Nouns
+                type: "SELECT",
+                order: 1,
+                question: 'Which one of these is "the man"',
+            }
+        ]);
+
+        await db.insert(schema.challengeOptions).values([
+            {
+                id: 1,
+                challengeId: 1,
+                imageSrc: "/man.svg",
+                correct: true,
+                text: "der Mann",
+                audioSrc: "/es_man.mp3",
+            },
+            {
+                id: 2,
+                challengeId: 1,
+                imageSrc: "/woman.svg",
+                correct: false,
+                text: "die Frau",
+                audioSrc: "/es_woman.mp3",
+            },
+            {
+                id: 3,
+                challengeId: 1,
+                imageSrc: "/robot.svg",
+                correct: false,
+                text: "der Roboter",
+                audioSrc: "/es_robot.mp3",
             },
         ])
 
