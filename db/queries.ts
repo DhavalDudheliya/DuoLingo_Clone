e4bc77223c18a593f33288ceb1b7fa88df76ae65
Dhaval Dudheliya <dhavaldudheliya77@gmail.com>
2024-08-26 14:11:41 +0530

    update

--- db/queries.ts
+++ db/queries.ts
@@ -2,7 +2,7 @@ import db from "./drizzle";
 import { cache, use } from "react";
 import { auth } from "@clerk/nextjs/server";
 import { eq } from "drizzle-orm";
-import { courses, userProgress, units, challengeProgress, lessons, challenges } from "./schema";
+import { courses, userProgress, units, challengeProgress, lessons, challenges, userSubscription } from "./schema";
 
 export const getUserProgress = cache(async () => {
     const { userId } = await auth();
@@ -175,6 +175,26 @@ export const getLessonPercentage = cache(async () => {
 
 
     return percentage;
+})
+
+const DAY_IN_MS = 86_400_000;
+export const getUserSubscription = cache(async () => {
+    const { userId } = await auth();
+
+    if (!userId) return null;
+
+    const data = await db.query.userSubscription.findFirst({
+        where: eq(userSubscription.userId, userId),
+    });
+
+    if (!data) return null;
+
+    const isActive = data.stripePriceId && data.stripeCurrentPeriodEnd?.getTime()! + DAY_IN_MS > Date.now();
+
+    return {
+        ...data,
+        isActive: !!isActive,
+    }
 })
 
 
